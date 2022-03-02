d9fed9086adcf09dec8d7e53536ee62a0e5515d2
ITSTIME <itstime0809@gmail.com>
2022-03-03 02:29:24 +0900

    Update : SplashScreen implementation :)

---
+++ osolna_application/lib/colorData/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// ignore: slash_for_doc_comments
+/**
+ * [Colors Data] this variable using all in the osolna application
+ * If you want to add color data, add it in this page. 
+ */
+
+Color mainbackgroundColor = const Color.fromRGBO(113, 92, 92, 100);
+Color logoColor = const Color.fromRGBO(215, 172, 21, 100);
+Color appbarColor = const Color.fromRGBO(98, 77, 77, 100);
+Color subtextColor = const Color.fromRGBO(189, 189, 189, 100);
+Color maintextColor = Colors.white;
---
+++ osolna_application/lib/textData/text.dart
@@ -0,0 +1,7 @@
+// ignore: slash_for_doc_comments
+/**
+ * [Fonts Data] this variable using all in the osolna application
+ * If you want to add font data, add it in this page
+ */
+String nanumMyeongjo = 'NanumMyeongjo';
+String nanumGothic = 'NanumGothic';
